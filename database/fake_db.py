09c2e99d1078fb3f7c210ca1e108d97711458058
Sam Teasdale <samuel.teasdale@gmail.com>
2015-11-25 20:53:31 -0500

    fake database classes fleshed out, tested

--- database/fake_db.py
+++ database/fake_db.py
@@ -1,3 +1,9 @@
+
+#  _   _ ___  ___ _ __ ___
+# | | | / __|/ _ \ '__/ __|
+# | |_| \__ \  __/ |  \__ \
+#  \__,_|___/\___|_|  |___/
+
 class User:
     def __init__(self, userid, first_name, last_name, groups = None):
         self.userid = userid
@@ -16,9 +22,7 @@ def allUsers():
 
 def userExistsByUserid(userid):
     user_list = [user for user in users if user.userid == userid]
-
-    if len(user_list) < 1:
-        raise LookupError("user with userid '{}' does not exist".format(userid))
+    return len(user_list) >= 1
 
 # Return a particular user that matches the userid
 def getUserByUserid(userid):
@@ -29,15 +33,53 @@ def getUserByUserid(userid):
 
     return user_list[0]
 
+# Add a new user
+def addUser(user):
+    if userExistsByUserid(user.userid):
+        raise ValueError("user with userid '{}' already exists")
 
+    # this'll raise an exception if group doesn't exist
+    for group in user.groups:
+        getGroupByName(group.name)
 
+    users.append(user)
 
+# Update an existing user.  Find the old user by id, make sure the new
+# user's group are legit, delete the old user object, append new one.
+def updateUser(new_user):
+    old_user = getUserByUserid(new_user.userid)
 
+    # yep, make sure these groups are legit
+    for group in new_user.groups:
+        getGroupByName(group.name)
 
+    users.remove(old_user)
+    users.append(new_user)
 
+# Delete an existing user
+def deleteUserByUserId(userid):
+    user = getUserByUserid(userid)
+    users.remove(user)
 
+def userHasGroup(user, group):
+    return len( [user_group for user_group in user.groups if user_group.name == group.name] ) > 0
 
+# Add a group to a user
+def addGroupToUser(user, group):
+    if not userHasGroup(user, group):
+        user.groups.append(group)
 
+# Remove a group from a user
+def removeGroupFromUser(user, group):
+    if userHasGroup(user, group):
+        user.groups.remove(group)
+
+#   __ _ _ __ ___  _   _ _ __  ___
+#  / _` | '__/ _ \| | | | '_ \/ __|
+# | (_| | | | (_) | |_| | |_) \__ \
+#  \__, |_|  \___/ \__,_| .__/|___/
+#   __/ |               | |
+#  |___/                |_|
 
 class Group:
     def __init__(self, name):
@@ -70,16 +112,41 @@ def getGroupByName(group_name):
         return group_list[0]
 
 def removeGroupByName(group_name):
-    if not groupNameExists(group_name):
+    removeGroup(getGroupByName(group_name))
+
+# Remove group from group list and from
+# users that are members of that group.
+def removeGroup(group):
+    if not groupExists(group):
+        raise LookupError("group '{}' does not exist".format(group.name))
+
+    groups.remove(group)
+    [removeGroupFromUser(user, group) for user in users]
+
+# Pass in a group and a list of userid strings
+def updateGroupMembership(group, userids):
+    if not groupNameExists(group.name):
         raise LookupError("group '{}' does not exist".format(group_name))
 
-    [groups.remove(group) for group in groups if group.name == group_name]
+    # Turn the list of userids into a list of users.  This'll throw
+    # an exception for any userid that doesn't match an actual user.
+    found_users = [getUserByUserid(userid) for userid in userids]
 
-def removeGroup(group):
-    removeGroupByName(group.name)
+    # Iterate through ALL users
+    for user in users:
+        if user in found_users:
+            addGroupToUser(user, group)
+        else:
+            removeGroupFromUser(user, group)
 
-
-
+#  _                 _       _                         _       _
+# | |               | |     | |                       | |     | |
+# | |__   ___   ___ | |_ ___| |_ _ __ __ _ _ __     __| | __ _| |_ __ _
+# | '_ \ / _ \ / _ \| __/ __| __| '__/ _` | '_ \   / _` |/ _` | __/ _` |
+# | |_) | (_) | (_) | |_\__ \ |_| | | (_| | |_) | | (_| | (_| | || (_| |
+# |_.__/ \___/ \___/ \__|___/\__|_|  \__,_| .__/   \__,_|\__,_|\__\__,_|
+#                                        | |
+#                                        |_|
 
 groups = [
     Group("users"),
@@ -102,4 +169,3 @@ users = [
         getGroupByName("pirates")
     ])
 ]
-
--- database/test_fake_db.py
+++ database/test_fake_db.py
@@ -1,12 +1,28 @@
 import database.fake_db as db
 import pytest
 
+#                        _            _
+#                       | |          | |
+#  _   _ ___  ___ _ __  | |_ ___  ___| |_ ___
+# | | | / __|/ _ \ '__| | __/ _ \/ __| __/ __|
+# | |_| \__ \  __/ |    | ||  __/\__ \ |_\__ \
+#  \__,_|___/\___|_|     \__\___||___/\__|___/
+#
+
 class TestFakeDbUsers():
 
     # Verify that allUsers returns our list of our three expected users.
     def test_allUsers(self):
         users = db.allUsers()
         assert len(users) == 3
+
+    # Verify that userExists returns true for an existing user, false otherwise
+    def test_userExistsById(self):
+        assert db.userExistsByUserid("jsmith")
+        assert db.userExistsByUserid("jjones")
+        assert db.userExistsByUserid("jsparrow")
+        assert not db.userExistsByUserid("pjiasasdf9")
+        assert not db.userExistsByUserid("ha-898ashdf")
 
     # Get a particular user by userid
     def test_getUserByUserid(self):
@@ -22,25 +38,121 @@ class TestFakeDbUsers():
         with pytest.raises(LookupError):
             db.getUserByUserid(";apoijpoiajsdf")
 
+    # Verify that we can add a new user with no groups
+    def test_addUser_no_groups(self):
+        user_count = len(db.allUsers())
+        db.addUser(db.User("u001", "joe", "user", [] ))
+        assert len(db.allUsers()) == (user_count + 1)
 
+    # Verify that we can add a new user with groups
+    def test_addUser(self):
+        user_count = len(db.allUsers())
+        db.addUser(db.User("u002", "jimmy", "user", [
+            db.Group("users"),
+            db.Group("pirates")
+        ]))
+        assert len(db.allUsers()) == (user_count + 1)
 
+    # Verify that addUser throws an exception when we attempt to
+    # add a user with the same userid as an existing user.
+    def test_addUser_conflict(self):
+        db.addUser(db.User("u003", "jack", "user"))
 
+        with pytest.raises(ValueError):
+            db.addUser(db.User("u003", "jack", "user"))
 
+    # Verify that addUser throws an exception when we
+    # attempt to add a user with groups that don't exist.
+    def test_addUser_bad_groups(self):
+        with pytest.raises(LookupError):
+            db.addUser(db.User("u004", "who", "cares", [db.Group("ap-8u9-8aojoia")]))
 
+    # Verify that updateUser throws an exception if we
+    # attempt to update a nonexistent user.
+    def test_updateUser_nonexistent(self):
+        with pytest.raises(LookupError):
+            db.updateUser(db.User("u005", "what", "ever"))
 
+    # Verify that updateUser throws an exception if
+    # one of this new user's groups doesn't already exist.
+    def test_updateUser_bad_groups(self):
+        db.addUser(db.User("u006", "tammy", "nguyen", [db.getGroupByName("admins")]))
 
+        with pytest.raises(LookupError):
+            db.updateUser(db.User("u006", "tammy", "nguyen", [db.Group("o089ujasdf")]))
 
+    # Verify that updateUser can actually update an existing user
+    def test_updateUser(self):
+        db.addUser(db.User("u007", "jeff", "hernandez", [
+            db.getGroupByName("users"),
+            db.getGroupByName("execs")
+        ]))
 
+        db.updateUser(db.User("u007", "joe", "gonzalez", [
+            db.getGroupByName("admins"),
+            db.getGroupByName("pirates")
+        ]))
 
+        user = db.getUserByUserid("u007")
 
+        assert user.userid == "u007"
+        assert user.first_name == "joe"
+        assert user.last_name == "gonzalez"
 
+        for group in user.groups:
+            assert group.name in ["admins", "pirates"]
 
+    # Can we delete a user?
+    def test_deleteUser(self):
+        user = db.User("u008", "trad", "miller")
 
+        db.addUser(user)
+        assert db.userExistsByUserid(user.userid)
 
+        db.deleteUserByUserId(user.userid)
+        assert not db.userExistsByUserid(user.userid)
 
+    # Verify that userHasGroup returns true if a user has a
+    # particular group in its group list, false otherwise.
+    def test_userHasGroup(self):
+        user = db.User("u009", "tronald", "dump", [
+            db.getGroupByName("execs"),
+            db.getGroupByName("pirates")
+        ])
 
+        assert db.userHasGroup(user, db.getGroupByName("execs"))
+        assert db.userHasGroup(user, db.getGroupByName("pirates"))
+        assert not db.userHasGroup(user, db.getGroupByName("admins"))
+        assert not db.userHasGroup(user, db.getGroupByName("users"))
 
+    # Can we add and remove groups from a user?
+    def test_addGroupToUser(self):
+        user = db.User("u010", "jimmy", "franco", [
+            db.getGroupByName("admins")
+        ])
 
+        assert len(user.groups) == 1
+        assert "users" not in [group.name for group in user.groups]
+
+        db.addGroupToUser(user, db.getGroupByName("users"))
+
+        assert len(user.groups) == 2
+        assert "admins" in [group.name for group in user.groups]
+        assert "users" in [group.name for group in user.groups]
+
+        db.removeGroupFromUser(user, db.getGroupByName("admins"))
+
+        assert len(user.groups) == 1
+        assert "admins" not in [group.name for group in user.groups]
+
+#                                _            _
+#                               | |          | |
+#   __ _ _ __ ___  _   _ _ __   | |_ ___  ___| |_ ___
+#  / _` | '__/ _ \| | | | '_ \  | __/ _ \/ __| __/ __|
+# | (_| | | | (_) | |_| | |_) | | ||  __/\__ \ |_\__ \
+#  \__, |_|  \___/ \__,_| .__/   \__\___||___/\__|___/
+#   __/ |               | |
+#  |___/                |_|
 
 class TestFakeDbGroups():
 
@@ -127,6 +239,21 @@ class TestFakeDbGroups():
         db.removeGroup(new_group)
         assert not db.groupNameExists("new_group_004")
 
+    # Verify that removeGroup removes the group from
+    # all users that are members of that group.
+    def test_removeGroup_from_users(self):
+        new_group = db.Group("sweet_new_group")
+        db.addGroup(new_group)
+
+        db.addUser(db.User("u020", "aaa", "aaa", [new_group]))
+        db.addUser(db.User("u021", "bbb", "bbb", [new_group]))
+
+        assert len( [user for user in db.allUsers() if new_group in user.groups] ) == 2
+
+        db.removeGroup(new_group)
+
+        assert len( [user for user in db.allUsers() if new_group in user.groups] ) == 0
+
     # Verify that removeGroupByName throws an exception
     # when we attempt to remove a nonexistent group
     def test_removeGroupByName_nonexistent(self):
@@ -138,3 +265,21 @@ class TestFakeDbGroups():
     def test_removeGroup_nonexistent(self):
         with pytest.raises(LookupError):
             db.removeGroup(db.Group("ppoijpaoij9a8us9d8pjasdf"))
+
+    # Verify that updateGroupMembership does that thing it's supposed to do.
+    def test_updateGroupMembership(self):
+        new_group = db.Group("even_sweeter_new_group")
+        db.addGroup(new_group)
+
+        db.addUser(db.User("u030", "aaa", "aaa", [db.getGroupByName("pirates"), new_group]))
+        db.addUser(db.User("u031", "bbb", "bbb", [db.getGroupByName("admins"), new_group]))
+        db.addUser(db.User("u032", "ccc", "ccc", [db.getGroupByName("execs")]))
+        db.addUser(db.User("u033", "ddd", "ddd", [db.getGroupByName("users")]))
+
+        for userid in [user.userid for user in db.allUsers() if new_group in user.groups]:
+            assert userid in ["u030", "u031"]
+
+        db.updateGroupMembership(new_group, ["u032", "u033"])
+
+        for userid in [user.userid for user in db.allUsers() if new_group in user.groups]:
+            assert userid in ["u032", "u033"]
